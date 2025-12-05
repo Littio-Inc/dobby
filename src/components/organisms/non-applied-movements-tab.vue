33518cd681300383b2ac1481d140010b22a9af22
Nico Ferretti <nicolas.ferretti@littio.co>
2025-12-05 16:16:18 -0300

    chore: added html from movement no apify

--- src/components/organisms/non-applied-movements-tab.vue
+++ src/components/organisms/non-applied-movements-tab.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="space-y-6">
-    <div class="bg-white rounded-lg border border-neutral-20 p-6">
-      <p class="text-neutral-60">Movimientos No Apificados - Próximamente</p>
-    </div>
-  </div>
+  <NonAppliedMovementForm />
 </template>
 
 <script setup lang="ts">
-// Componente vacío por ahora
+import NonAppliedMovementForm from './non-applied-movement-form.vue';
 </script>
