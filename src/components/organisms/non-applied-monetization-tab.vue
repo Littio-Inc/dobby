91fa2a2a7ba1a201d99da4c6485cbc14c7970b6c
nicoferre-littio <nicolas.ferretti@littio.co>
2025-12-22 16:03:32 -0300

    ⚡ chore: added tab monetizacion no apificados (#17)
    * chore: added tab monetizacion no apificados
    
    * chore: added tab monetizacion no apificados

---
+++ src/components/organisms/non-applied-monetization-tab.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="space-y-8">
+    <div class="bg-white rounded-lg border border-neutral-20 p-8 text-center">
+      <p class="text-neutral-60 text-lg">El formulario de Monetización No Apificados estará disponible próximamente.</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Este componente estará listo cuando se defina el formulario de monetización no apificados
+// Props se agregarán cuando se defina el formulario
+</script>
